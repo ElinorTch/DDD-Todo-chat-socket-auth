ee5142a173b781358194d3d7a44eff44cd6ef025
Tchamo Elinor <tchamoelii@gmail.com>
2025-05-28 22:18:27 +0100

    Project initialization

---
+++ Backend/src/main/java/com/example/chattrix/ChattrixApplication.java
@@ -0,0 +1,13 @@
+package com.example.chattrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChattrixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChattrixApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/example/chattrix/ChattrixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chattrix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChattrixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
